93fb1ae93a2576b70e1ede3927b2385ab7d1f77c
Dominik Broj <dominik.broj@dataart.com>
2017-03-08 15:28:24 +0100

    apply thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { devToolsEnhancer  } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools  } from 'redux-devtools-extension';
 import routes from './routes';
 import wannaPlayApp from './reducers';
 import './global_styles/index.css';
 
-let store = createStore(wannaPlayApp, devToolsEnhancer());
+let store = createStore(wannaPlayApp, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
