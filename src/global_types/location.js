a5a3c87542f6f75d7d095299e655dba4099eb17a
Dominik Broj <dominik.broj@dataart.com>
2017-03-13 10:49:54 +0100

    save current location in store when user logs in

---
+++ src/global_types/location.js
@@ -0,0 +1,3 @@
+declare type Location = {
+  currentLocation: string
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import users from './users';
 import places from './places';
 import facebook from './facebook';
 import currentUser from './currentUser';
+import location from './location';
 
 const wannaPlayApp = combineReducers({
   games,
   users,
   places,
   facebook,
-  currentUser
+  currentUser,
+  location
 });
 
 export default wannaPlayApp;
